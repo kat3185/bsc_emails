e4fcb1c56e23c116aa71b10e295c90b269f3635e
Kenneth Thomas <Ken@Kenneths-MacBook-Pro.local>
2017-03-16 04:48:40 -0400

    indexeddb working

--- view.js
+++ view.js
@@ -1,16 +1,38 @@
+const DB_NAME = "bsc_mail";
+const DB_VERSION = 1;
+const DB_STORE_NAME = "emails";
+
 
 
 $('form').submit(function(event) {
   event.preventDefault();
   console.log("Form submission prevented.");
+  var signupEmail = $('.element').val();
 
-  //save form information ->
-    //flash thank you message ->
-    //clear form and message
-    $('.element').val('');
-    $('.thankYouMessage').show(function() {
-      $('.thankYouMessage').fadeOut(1500, function() {
-
+  localforage.getItem('emails').then(function(value) {
+      var emails = value || [];
+      emails.push(signupEmail);
+      console.log('value');
+      return emails;
+  }).catch(function(err) {
+      // This code runs if there were any errors
+      console.log('err');
+  }).then(function(emails) {
+    localforage.setItem('emails', emails).then(function (value) {
+      $('.element').val('');
+      $('.thankYouMessage').show(function() {
+        $('.element').val('');
+        signupEmail = "";
+        $('.thankYouMessage').fadeOut(1500, function() {});
       });
+        console.log(emails);
+    }).catch(function(err) {
+        // This code runs if there were any errors
+        console.log(err);
     });
+  });
+
+
+
+
 });
