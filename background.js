3f6f8dcf82e048786157aa9b75ab30d8fa6a8677
Kenneth Thomas <Ken@Kenneths-MacBook-Pro.local>
2017-03-17 18:52:11 -0400

    show and clear functions added

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('form.html', {
     'outerBounds': {
       'width': 800,
-      'height': 370
+      'height': 600
     }
   });
 });
--- view.js
+++ view.js
@@ -2,21 +2,24 @@ const DB_NAME = "bsc_mail";
 const DB_VERSION = 1;
 const DB_STORE_NAME = "emails";
 
-
+document.onkeydown=function() {
+    if(window.event.keyCode=='13'){
+        $('form').submit();
+    }
+};
 
 $('form').submit(function(event) {
   event.preventDefault();
   console.log("Form submission prevented.");
-  var signupEmail = $('.element').val();
+  var signupEmail = $('.email_entry').val();
 
   localforage.getItem('emails').then(function(value) {
       var emails = value || [];
       emails.push(signupEmail);
-      console.log('value');
       return emails;
   }).catch(function(err) {
       // This code runs if there were any errors
-      console.log('err');
+      console.log(err);
   }).then(function(emails) {
     localforage.setItem('emails', emails).then(function (value) {
       $('.element').val('');
@@ -25,14 +28,44 @@ $('form').submit(function(event) {
         signupEmail = "";
         $('.thankYouMessage').fadeOut(1500, function() {});
       });
-        console.log(emails);
     }).catch(function(err) {
         // This code runs if there were any errors
         console.log(err);
     });
   });
+});
 
+$('#show_emails').click(function(event) {
+  var password = "showmetheemails";
+  var passwordAttempt = $('.password_attempt').val();
+  if (password === passwordAttempt) {
+    localforage.getItem('emails').then(function(value) {
+        var emails = value || [];
+        if (emails.length === 0) {
+          emails.push("No Emails Present");
+        }
+        return emails;
+    }).catch(function(err) {
+        var emails = [];
+        if (emails.length === 0) {
+          emails.push("No Emails Present");
+        }
+        console.log(err);
+        return emails;
+    })
 
+    .then(function(emails) {
+      $('.email_list').html(emails.join()).fadeIn(1500, function() {});
+      $('#delete_emails').fadeIn(1500, function() {});
+    });
+  } else {
+    $('.email_list').html('That password was incorrect.  You bad.').fadeIn(1500, function() {});
+    $('#delete_emails').fadeOut(1500, function() {});
+  }
+});
 
-
+$('#delete_emails').click(function(event) {
+  localforage.clear();
+  $('.email_list').fadeOut(1500, function() {});
+  $('#delete_emails').fadeOut(1500, function() {});
 });
