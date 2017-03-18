785f1139e969c247e972f6d8c30553c3762bcccb
Kenneth Thomas <Ken@Kenneths-MacBook-Pro.local>
2017-03-18 14:24:50 -0400

    refactor to remove admin button

--- view.js
+++ view.js
@@ -1,50 +1,46 @@
-const DB_NAME = "bsc_mail";
-const DB_VERSION = 1;
-const DB_STORE_NAME = "emails";
-
 document.onkeydown=function() {
     if(window.event.keyCode=='13'){
-      if ($('.password_attempt').is(':focus')){
-        $('#show_emails').click();
-      } else {
-        $('form').submit();
-      }
+      $('form').submit();
     }
 };
 
+$('#saveForm').click(function(event) {
+  $('form').submit();
+});
 
 $('form').submit(function(event) {
   event.preventDefault();
-  console.log("Form submission prevented.");
   var signupEmail = $('.email_entry').val();
-
-  localforage.getItem('emails').then(function(value) {
-      var emails = value || [];
-      emails.push(signupEmail);
-      return emails;
-  }).catch(function(err) {
-      // This code runs if there were any errors
-      console.log(err);
-  }).then(function(emails) {
-    localforage.setItem('emails', emails).then(function (value) {
-      $('.element').val('');
-      $('.thankYouMessage').show(function() {
-        $('.element').val('');
-        signupEmail = "";
-        $('.thankYouMessage').fadeOut(1500, function() {});
-      });
-    }).catch(function(err) {
-        // This code runs if there were any errors
-        console.log(err);
-    });
-  });
+  let handler = new FormSubmissionHandler(signupEmail);
+  handler.handleSubmit();
 });
 
-$('#show_emails').click(function(event) {
-  var password = "showmetheemails";
-  var passwordAttempt = $('.password_attempt').val();
-  if (password === passwordAttempt) {
-    localforage.getItem('emails').then(function(value) {
+$('#delete_emails').click(function(event) {
+  localforage.removeItem('emails');
+  $('.show_emails').fadeOut(1500, function() {});
+});
+
+class FormSubmissionHandler {
+  constructor(signupEmail) {
+    this.signupEmail = signupEmail;
+    this.password = "showmetheemails";
+    this.dbName = "emails";
+  }
+
+  handleSubmit() {
+    var that = this;
+    $('.show_emails').fadeOut(1500, function() {});
+    console.log('submit handled!');
+    if (that.signupEmail === that.password) {
+      that.showEmails(that);
+    } else {
+      console.log('about to save the email, sir!');
+      that.saveEmail(that);
+    }
+  }
+
+  showEmails(that) {
+    localforage.getItem(that.dbName).then(function(value) {
         var emails = value || [];
         if (emails.length === 0) {
           emails.push("No Emails Present");
@@ -60,19 +56,38 @@ $('#show_emails').click(function(event) {
     })
 
     .then(function(emails) {
-      $('.email_list').html(emails.join()).fadeIn(1500, function() {});
+      $('.email_list').html(emails.join());
+      $('.show_emails').fadeIn(1500, function() {});
       $('#delete_emails').fadeIn(1500, function() {});
-      $('.password_attempt').val('');
+      $('.element').val('');
     });
-  } else {
-    $('.email_list').html('That password was incorrect.  You bad.').fadeIn(1500, function() {}).fadeOut(3000, function() {});
-    $('#delete_emails').fadeOut(1500, function() {});
-    $('.password_attempt').val('');
   }
-});
 
-$('#delete_emails').click(function(event) {
-  localforage.removeItem('emails');
-  $('.email_list').fadeOut(1500, function() {});
-  $('#delete_emails').fadeOut(1500, function() {});
-});
+  saveEmail(that) {
+    console.log('start of email save!');
+    localforage.getItem(that.dbName).then(function(value) {
+      console.log("I got mah item!");
+        var emails = value || [];
+        if (that.signupEmail !== "") { emails.push(that.signupEmail); }
+        console.log("Found teh emails");
+        return emails;
+    }).catch(function(err) {
+        // This code runs if there were any errors
+        console.log(err);
+    }).then(function(emails) {
+      console.log(emails);
+      localforage.setItem(that.dbName, emails).then(function (value) {
+        console.log(emails);
+        $('.element').val('');
+        $('.thankYouMessage').show(function() {
+          $('.element').val('');
+          $('.thankYouMessage').fadeOut(1500, function() {});
+        });
+      }).catch(function(err) {
+          // This code runs if there were any errors
+          console.log(err);
+      });
+    });
+  }
+
+}
